0d857bf23803d704e73da4f380f0e4ea60b97544
Min Kim <8450minkim@gmail.com>
2023-04-21 15:00:17 -0700

    added insert

--- main.cpp
+++ main.cpp
@@ -7,27 +7,45 @@ using namespace std;
 class Node {
     public:
     int data;
+    Node* parent;
     Node* left;
     Node* right;
+    int type;
     Node(int num) {
         data = num;
+        parent = NULL;
         left = NULL;
         right = NULL;
+        type = 1;//red is 1, black is 2
     }
 };
 
+void insert(Node* &newnode, Node* &head);
+//void insert-fix(Node* &newnode, Node* &head)
+void print(Node* head, int numTabs);
+
 int main() {
+    cout << 1/5;
     char input[80];
     int rawinputs[80];
     bool cont = true;
     int total = 0;
+    int num = 0;
     Node* head = NULL;
     while (cont == true) {
         cout << "Add, read, or print (add/read/print)?: ";
         cin >> input;
         cin.ignore();
         if (strcmp(input, "add") == 0) {
-
+            while(num != -1) {
+                cout << "Enter number, enter -1 to quit adding: ";
+                 cin >> num;
+                if (num == -1) {
+                    break;
+                }
+            Node* newnode = new Node(num);
+            insert(newnode, head);
+        }
         }
         else if (strcmp(input, "read") == 0) {
             char file[80];
@@ -40,7 +58,7 @@ int main() {
             cout << "Numbers from file: ";
             while (numFile >> rawinputs[total]) {
                 Node* newnode = new Node(rawinputs[total]);
-                insert(newnode, head);
+                //insert(newnode, head);
                 cout << rawinputs[total] << " ";
                 total+=1;
             }
@@ -56,7 +74,38 @@ int main() {
 
 }
 
-void insert() {
+void insert(Node* &newnode, Node* &head) {
+    if (head == NULL) {//if there is nothing in the tree
+        head = newnode;
+        head->type = 2;
+    }
+    else {
+        Node* n = head;
+        while((n->left != NULL) || (n->right != NULL)) {
+            if (newnode->data > n->data) {
+                n = n->right;
+            }
+            else {
+                n = n->left;
+            }
+            cout << n->data << endl;
+        }
+        cout << n->data << endl;
+        newnode->parent = n;
+        if (n->data > newnode->data) {
+            n->left = newnode;
+        }
+        else {
+            n->right = newnode;
+        }
+        newnode->type = 1;
+        //insert_fix();
+    }
+    int numTabs = 0;
+    print(head, numTabs);
+}
+
+void insert_fix() {
 
 }
 
